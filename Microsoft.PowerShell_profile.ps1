183a2a150ea90a6b82cdef5a65ca6a8ad7cc7ef6
Louis Munoz <nurdoidz@live.com>
2023-12-16 00:27:24 -0700

    Add vim and dev commands

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,6 @@
 # symbolic links
-function Make-Link($target, $link) {
-    New-Item -Path $link -ItemType SymbolicLink -Value $target
+Function New-Link($Target, $Link) {
+    New-Item -Path $Link -ItemType SymbolicLink -Value $Target
 }
 
 # bash-like auto-complete
@@ -8,19 +8,72 @@ Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
 # [vim] -->
 New-Alias -Name vim -Value nvim
+New-Alias -Name vd -Value vimdev
+New-Alias -Name codev -Value codedev
 # edit the neovim config
-function vimcon {
-    $currentDir = Get-Location
+Function vimcon {
+    $CurrentDir = Get-Location
     Set-Location $env:localappdata\nvim
     vim .
-    Set-Location $currentDir
+    Set-Location $CurrentDir
 }
-function dev {
-    Set-Location C:\Dev
+Function vimprofile {
+    $CurrentDir = Get-Location
+    Split-Path -Parent $Profile | Set-Location
+    vim $Profile
+    Set-Location $CurrentDir
+}
+Function codeprofile {
+    $CurrentDir = Get-Location
+    Split-Path -Parent $Profile | Set-Location
+    code $Profile
+    Set-Location $CurrentDir
+}
+Function dev {
+    param([string]$P, [switch]$C, [switch]$V)
+    $Path = 'C:\Dev'
+    if ($PSBoundParameters.ContainsKey('P')) {
+        $Path = "$Path\$P"
+    }
+    Set-Location $Path
+    if ($C) {
+        code .
+    }
+    if ($V) {
+        vim .
+    }
+}
+Function vimdev {
+    param([string]$P)
+    dev -P $P -V
+}
+Function codedev {
+    param([string]$P)
+    dev -P $P -C
+}
+$ProjectCompleter = {
+    param($CommandName, $ParameterName, $WordToComplete, $CommandAst, $FakeBoundParameter)
+
+    $Path = 'C:\Dev'
+    $Dirs = Get-ChildItem -Path $Path -Directory | Select-Object -ExpandProperty Name
+
+    return $Dirs | Where-Object { $_ -like "$WordToComplete*" }
+}
+
+Register-ArgumentCompleter -CommandName vimdev -ParameterName P -ScriptBlock $ProjectCompleter
+Register-ArgumentCompleter -CommandName dev -ParameterName P -ScriptBlock $ProjectCompleter
+
+Function cdls {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$Path
+    )
+    Set-Location $Path
+    Get-ChildItem
 }
 
 # [komorebi] -->
-function komo {
+Function komo {
     komorebic start -a
 }
 $Env:KOMOREBI_CONFIG_HOME = $env:ndz + '\com\komorebi'
